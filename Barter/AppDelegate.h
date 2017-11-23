b9f60a2da00fbc1be346d0bbeb8310862139b544
Nikhil Capsitech <nikhilcapsitech@nikhils-imac.local>
2017-11-23 14:12:19 +0530

    Initial Commit

---
+++ Barter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Barter
+//
+//  Created by Nikhil Capsitech on 23/11/17.
+//  Copyright © 2017 Nikhil Capsitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Barter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barter
+//
+//  Created by Nikhil Capsitech on 23/11/17.
+//  Copyright © 2017 Nikhil Capsitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
